4610fdbb48647dcf26890f01b832c19acf631bd6
guzzmle <73357188+guzzmle@users.noreply.github.com>
2021-12-05 22:30:40 +0000

    Rectified email and password validation and cleaned code

--- src/main/java/com/example/demo/FormController.java
+++ src/main/java/com/example/demo/FormController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Window;
@@ -21,21 +20,35 @@ public class FormController {
     private Button submitButton;
 
     @FXML
-    protected void buttonHandler(ActionEvent event) {
+    protected void buttonHandler() {
         Window wind = submitButton.getScene().getWindow();
         String message = "Welcome " + nameField.getText();
         boolean fieldError = true;
 
-        if(!(nameField.getText().isEmpty())) {
+        if((passwordField.getText().length() >= 7) &&
+                passwordField.getText().matches(".*[a-zA-Z].*") &&
+                passwordField.getText().matches(".*[0-9].*") &&
+                (
+                        passwordField.getText().contains("*") ||
+                                passwordField.getText().contains("^") ||
+                                passwordField.getText().contains("&") ||
+                                passwordField.getText().contains("@") ||
+                                passwordField.getText().contains("!")
+                )) {
             fieldError = false;
         } else {
-            message = "Please enter your name";
+            message = """
+                    Please enter a stronger password.\s
+                    TIP: a strong password has to be at least 7 characters in length,\s
+                    contains at least 1 letter,\s
+                    contains at least 1 digit,\s
+                    and contains at least 1 character from *^&@!""";
         }
 
-        if(!(emailField.getText().isEmpty())) {
+        if(emailField.getText().matches("(.*)@(.*).com") || emailField.getText().matches("(.*)@(.*).ie")) {
             fieldError = false;
         } else {
-            message = "Please enter your email address";
+            message = "Please enter a valid email address";
         }
 
         if(!(passwordField.getText().isEmpty())) {
@@ -44,23 +57,16 @@ public class FormController {
             message = "Please enter your password";
         }
 
-        if(emailField.getText().matches("(.*)@(.*).com")) {
+        if(!(emailField.getText().isEmpty())) {
             fieldError = false;
         } else {
-            message = "Please enter a valid email address";
+            message = "Please enter your email address";
         }
 
-        if((passwordField.getText().length() >= 7) &&
-                passwordField.getText().matches("[abcdefghijklmnopqrstuvwxyz]") &&
-                passwordField.getText().matches("[0123456789]") &&
-                passwordField.getText().matches("[&@!]")) {
+        if(!(nameField.getText().isEmpty())) {
             fieldError = false;
         } else {
-            message = "Please enter a stronger password. \n" +
-                    "TIP: a strong password has to be at least 7 characters in length, \n" +
-                    "contains at least 1 letter, \n" +
-                    "contains at least 1 digit, \n" +
-                    "and contains at least 1 character from *^&@!";
+            message = "Please enter your name";
         }
 
         if(fieldError) {
